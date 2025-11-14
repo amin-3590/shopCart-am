98075c00f7e646588efe70ddfec81a007b030a65
amin aliakbari <lyakbryamyn7@gmail.com>
2025-11-14 22:42:57 +0330

    cart page section complete

--- src/components/AddToWishListButton.tsx
+++ src/components/AddToWishListButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Heart } from "lucide-react";
 import { Product } from "../../sanity.types";
 import { cn } from "@/lib/utils";
@@ -11,7 +13,7 @@ export default function AddToWishListButton({ product, className }: { product: P
     const [existingProduct, setExistingProduct] = useState<Product | null>(null);
 
     useEffect(() => {
-        const availableProduct = favoriteProduct?.find((item) => item?._id === product?._id);
+        const availableProduct = favoriteProduct.find((item) => item._id === product._id);
 
         setExistingProduct(availableProduct || null);
     }, [product, favoriteProduct]);
@@ -27,10 +29,10 @@ export default function AddToWishListButton({ product, className }: { product: P
     };
 
     return (
-        <div className={cn('absolute top-2 right-2 z-10', className)}>
-            <button onClick={handleFavorite} className={`p-2.5 rounded-full hover:bg-shop_dark_green/80 hover:text-white transition-all duration-300 ease-in-out  ${existingProduct ? "bg-shop_dark_green/80 text-white" : "bg-lightColor/10"}`}>
+        <div className={cn('absolute top-2 right-2 cursor-pointer', className)}>
+            <div onClick={handleFavorite} className={`p-2.5 rounded-full hover:bg-shop_dark_green/80 hover:text-white transition-all duration-300 ease-in-out  ${existingProduct ? "bg-shop_dark_green/80 text-white" : "bg-lightColor/10"}`}>
                 <Heart size={15} />
-            </button>
+            </div>
         </div>
     );
 }
